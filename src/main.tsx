efcc5f526e8ea8e99aba9a920105c9a56ae4afac
DESKTOP-8IENF4O\User <yourdeveloper@mail.ru>
2024-02-08 08:07:32 +0700

    add redux, storeprovider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import {createRoot} from 'react-dom/client'
 import App from './app/App.tsx'
 import {BrowserRouter} from "react-router-dom";
 import './app/styles/index.scss';
+import {StoreProvider} from "./app/providers/StoreProvider";
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
 root.render(
 		<BrowserRouter>
-			<App />
+			<StoreProvider>
+				<App />
+			</StoreProvider>
 		</BrowserRouter>
 );
